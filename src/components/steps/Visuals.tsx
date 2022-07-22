fc3dfc8367c73e84133fdd779d42659fab1c62d6
KayodeOusi <kayodeolusi@gmail.com>
2022-07-22 16:04:01 +0100

    work on templates and stepper next

--- src/components/steps/Visuals.tsx
+++ src/components/steps/Visuals.tsx
@@ -4,29 +4,37 @@ import { svgs } from "../../constants";
 const Visuals: FC = () => {
   return (
     <div
-      className="mt-14 grid grid-rows-3 grid-flow-col gap-4
-     lg:grid-cols-5 lg:grid-rows-1 lg:grid-flow-row"
+      className="mt-14 mb-16 grid grid-rows-3 grid-flow-col gap-12
+     lg:grid-cols-5 lg:grid-rows-1 lg:grid-flow-row lg:gap-4 lg:mb-0"
     >
       <div className="text-center flex flex-col space-y-4 items-center">
-        <img src={svgs.template} alt="t" />
+        <img
+          src={svgs.template}
+          alt="t"
+          className="h-32 w-32 lg:h-fit lg:w-fit"
+        />
         <h6 className="font-semibold text-herotext text-xl">Step 1</h6>
-        <p className="text-sm font-semibold text-hero">
+        <p className="text-sm font-semibold text-hero px-12 sm:px-16 md:px-0">
           Select your preferred template
         </p>
       </div>
-      <hr className="bg-alium w-56 h-2 mt-20 hidden lg:inline-block" />
+      <hr className="bg-alium h-2 mt-20 hidden lg:inline-block lg:w-44 xl:w-56" />
       <div className="text-center flex flex-col space-y-4 items-center">
-        <img src={svgs.edit} alt="e" />
+        <img src={svgs.edit} alt="e" className="h-32 w-32 lg:h-fit lg:w-fit" />
         <h6 className="font-semibold text-herotext text-xl">Step 2</h6>
-        <p className="text-sm font-semibold text-hero">
+        <p className="text-sm font-semibold text-hero px-14 sm:px-16 md:px-0">
           Fill using your details.
         </p>
       </div>
-      <hr className="bg-alium w-56 h-2 hidden mt-20 lg:inline-block" />
+      <hr className="bg-alium h-2 hidden mt-20 lg:inline-block lg:w-44 xl:w-56" />
       <div className="text-center flex flex-col space-y-4 items-center">
-        <img src={svgs.download} alt="d" />
+        <img
+          src={svgs.download}
+          alt="d"
+          className="h-32 w-32 lg:h-fit lg:w-fit"
+        />
         <h6 className="font-semibold text-herotext text-xl">Step 3</h6>
-        <p className="text-sm font-semibold text-hero">
+        <p className="text-sm font-semibold text-hero px-12 sm:px-16 md:px-0">
           Download and stun your employers.
         </p>
       </div>
--- src/pages/steps/index.tsx
+++ src/pages/steps/index.tsx
@@ -1,17 +1,21 @@
+import { FC } from "react";
 import Visuals from "../../components/steps/Visuals";
 
-const Steps = () => {
+const Steps: FC = () => {
   return (
-    <div className="mt-20">
-      <h1 className="text-center text-alium font-semibold text-4xl">
+    <div className="mt-20 px-8 lg:px-0">
+      <h1 className="text-center text-alium font-semibold text-2xl lg:text-4xl">
         Let us get you your dream job in 3 steps.
       </h1>
       <p className="text-center text-herotext font-semibold text-base mt-5">
         It is as easy as 1, 2, yes, you get the point. Shall we?
       </p>
       <Visuals />
-      <div className="mt-4 text-center md:mt-8 lg:mt-20">
-        <button className="bg-herobtn rounded-sm text-white px-6 py-4 mb-6 text-xs md:mb-0 md:px-5 xl:px-10 xl:py-6">
+      <div className="text-center md:mt-8 lg:mt-20">
+        <button
+          className="bg-herobtn rounded-sm text-white px-6 py-4 mb-6 text-xs
+         md:mb-0 md:px-5 xl:px-10 xl:py-6"
+        >
           Build My Resume
         </button>
       </div>
